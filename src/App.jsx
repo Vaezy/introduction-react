d972536c97d993caffe77b5853a430a86b5d86cf
Thomas <boy1.thoomas@gmail.com>
2025-03-04 17:13:21 +0100

    feat(dish): add a table to store dish data

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,48 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
 export const App = () => {
+  const dishes = [
+    {
+      id: 1,
+      name: "Tacos à l'unité",
+      price: 3,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      isNew: true,
+    },
+    {
+      id: 2,
+      name: "Enchiladas",
+      price: 12,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      isNew: false,
+    },
+    {
+      id: 3,
+      name: "Mole poblano",
+      price: 3,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      isNew: false,
+    },
+  ];
   return (
     <>
       <Header />
       <main className="pt-5 pb-5">
         <Container>
           <Row>
-            <Col md={4}>
-              <Dish
-                name="Tacos à l'unité"
-                price={3}
-                imgSrc="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                isNew={true}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Enchiladas"
-                price={12}
-                imgSrc="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Mole poblano"
-                price={3}
-                imgSrc="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
+            {dishes.map((dish) => (
+              <Col key={dish.id} md={4}>
+                <Dish
+                  name={dish.name}
+                  price={dish.price}
+                  imgSrc={dish.imgSrc}
+                  isNew={dish.isNew}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
