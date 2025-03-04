e8f1eccccb5e740bff593b253739e5047ae8ead9
Thomas <boy1.thoomas@gmail.com>
2025-03-04 15:10:24 +0100

    feat(dish): add a badge if the dish is new

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export const App = () => {
                 name="Tacos à l'unité"
                 price={3}
                 imgSrc="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+                isNew={true}
               />
             </Col>
             <Col md={4}>
@@ -22,6 +23,7 @@ export const App = () => {
                 name="Enchiladas"
                 price={12}
                 imgSrc="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+                isNew={false}
               />
             </Col>
             <Col md={4}>
@@ -29,6 +31,7 @@ export const App = () => {
                 name="Mole poblano"
                 price={3}
                 imgSrc="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+                isNew={false}
               />
             </Col>
           </Row>
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,11 +1,17 @@
+import Badge from "react-bootstrap/Badge";
 import Card from "react-bootstrap/Card";
 import "../assets/styles/dish.scss";
 
-export const Dish = ({ name, price, imgSrc }) => {
+export const Dish = ({ name, price, imgSrc, isNew }) => {
   return (
     <Card>
       <Card.Img src={imgSrc} />
       <Card.Body>
+        {isNew ? (
+          <Badge bg="primary position-absolute top-0 end-0 m-2">Nouveau</Badge>
+        ) : (
+          ""
+        )}
         <Card.Title>{name}</Card.Title>
         <Card.Text>Prix : {price}€</Card.Text>
       </Card.Body>
