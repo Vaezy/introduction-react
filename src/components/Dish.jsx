ef6d80cea27bf22fab30c8e46b0e29f796b12068
Thomas <boy1.thoomas@gmail.com>
2025-03-04 17:35:45 +0100

    feat(dish): add button to add to cart

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,4 +1,5 @@
 import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "../assets/styles/dish.scss";
 
@@ -8,10 +9,15 @@ export const Dish = ({ name, price, imgSrc, isNew }) => {
       <Card.Img src={imgSrc} />
       <Card.Body>
         {isNew === true && (
-          <Badge bg="primary position-absolute top-0 end-0 m-2">Nouveau</Badge>
+          <Badge bg="primary" className="position-absolute top-0 end-0 m-2">
+            Nouveau
+          </Badge>
         )}
         <Card.Title>{name}</Card.Title>
         <Card.Text>Prix : {price}€</Card.Text>
+        <Button variant="primary" className="w-100">
+          Ajouter au panier
+        </Button>
       </Card.Body>
     </Card>
   );
