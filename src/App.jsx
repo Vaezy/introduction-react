538cea4a524e5d970955831b324e9d7ca93968ce
Thomas <boy1.thoomas@gmail.com>
2025-03-04 17:18:24 +0100

    feat(dish): add stock data

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export const App = () => {
       id: 1,
       name: "Tacos à l'unité",
       price: 3,
+      stock: 12,
       imgSrc:
         "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       isNew: true,
@@ -17,6 +18,7 @@ export const App = () => {
       id: 2,
       name: "Enchiladas",
       price: 12,
+      stock: 0,
       imgSrc:
         "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       isNew: false,
@@ -25,18 +27,22 @@ export const App = () => {
       id: 3,
       name: "Mole poblano",
       price: 3,
+      stock: 5,
       imgSrc:
         "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       isNew: false,
     },
   ];
+
+  const isInStock = dishes.filter((dish) => dish.stock);
+
   return (
     <>
       <Header />
       <main className="pt-5 pb-5">
         <Container>
           <Row>
-            {dishes.map((dish) => (
+            {isInStock.map((dish) => (
               <Col key={dish.id} md={4}>
                 <Dish
                   name={dish.name}
